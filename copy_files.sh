2f24136d3e16e9af4d043a20070d848a8ab7eee9
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-10-09 16:11:13 +0300

    [FEATURE] Add GnuPG encryption for temporary files with keys

--- copy_files.sh
+++ copy_files.sh
@@ -2,4 +2,4 @@
 chmod +x *
 cp ./yubikey /usr/share/initramfs-tools/hooks/yubikey
 cp ./yubikey_unlock /lib/cryptsetup/scripts/yubikey_unlock
-cp ./yubico_refresh /usr/share/initramfs-tools/scripts/local-bottom/yubico_refresh
+cp ./yubico_refresh /usr/share/initramfs-tools/scripts/init-bottom/yubico_refresh
